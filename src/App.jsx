642d5372cf05a94d32828bdf53a17c7b9c01181e
philiprockert <philiprockert@gmail.com>
2023-10-15 12:59:10 -0500

    organizando navegavilidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { Login } from './login';
-import { Internav } from './Internav';
+
 import { Dashboard } from './dashboard';
 import Report from './report';
 import Sends from './sends';
